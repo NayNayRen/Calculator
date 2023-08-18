942dcf3e99374cc7f234bb36e17a98749e4ecf30
Nate <jeditendencies35@gmail.com>
2023-08-18 14:25:19 -0400

    added solar, description, and better eval checks with responses

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -3,11 +3,22 @@ function insert(num) {
 }
 
 function equals() {
-  const num = document.calculator.output.value;
-  if (num) {
-    document.calculator.output.value = eval(num);
+  const expression = document.calculator.output.value;
+  try {
+    eval(expression);
+    if (eval(expression) === undefined) {
+      return document.calculator.output.value = 'Empty Input...'
+    } else {
+
+      return document.calculator.output.value = eval(expression);
+    }
+  }
+  catch (error) {
+    // console.log(error);
+    return document.calculator.output.value = 'Not Valid...';
   }
 }
+
 function back() {
   const num = document.calculator.output.value;
   document.calculator.output.value = num.substring(0, num.length - 1);
