5c1dbf9c634349afe9abfa4e94b3963ab661b7d1
Nate <jeditendencies35@gmail.com>
2023-08-18 18:29:02 -0400

    added a bunch of styles, animation box with expression, reworked button code

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,5 +1,16 @@
+const backButton = document.querySelector('.back-btn');
+const equalsButton = document.querySelector('.equals-btn');
+const outputEntered = document.querySelector('.output-entered');
+const clearButton = document.querySelector('.clear-btn');
+let buttons = document.querySelectorAll('.btn');
+
 function insert(num) {
   document.calculator.output.value = document.calculator.output.value + num;
+}
+
+function back() {
+  const num = document.calculator.output.value;
+  document.calculator.output.value = num.substring(0, num.length - 1);
 }
 
 function equals() {
@@ -9,7 +20,11 @@ function equals() {
     if (eval(expression) === undefined) {
       return document.calculator.output.value = 'Empty Input...'
     } else {
-
+      outputEntered.innerHTML = `
+      <span>Expression</span>
+      <p>${document.calculator.output.value}</p>
+      `;
+      outputEntered.classList.add('output-entered-action');
       return document.calculator.output.value = eval(expression);
     }
   }
@@ -19,7 +34,21 @@ function equals() {
   }
 }
 
-function back() {
-  const num = document.calculator.output.value;
-  document.calculator.output.value = num.substring(0, num.length - 1);
-}
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    insert(button.value);
+  });
+});
+
+clearButton.addEventListener('click', () => {
+  outputEntered.classList.remove('output-entered-action');
+  document.calculator.output.value = '';
+});
+
+backButton.addEventListener('click', () => {
+  back();
+});
+
+equalsButton.addEventListener('click', () => {
+  equals();
+});
